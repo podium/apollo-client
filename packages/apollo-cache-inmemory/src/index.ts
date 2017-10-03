c4957469c0bf69f9771d2d68823b420f5e2b2a23
Valery Bugakov <skymk1@gmail.com>
2017-10-03 12:17:09 +0300

    Included built Apollo-Client package

--- packages/apollo-cache-inmemory/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { InMemoryCache, defaultDataIdFromObject } from './inMemoryCache';
-export * from './readFromStore';
-export * from './writeToStore';
-export * from './fragmentMatcher';
-export * from './types';
-export { defaultDataIdFromObject };
-export default InMemoryCache;
